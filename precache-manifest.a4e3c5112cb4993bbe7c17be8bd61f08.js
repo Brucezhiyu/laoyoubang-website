f11ddeb86f3a4ce962d4201a93c7056d3664c87c
luzhiyu <lushuke5@gmail.com>
2022-04-24 15:12:49 +0800

    deploy

---
+++ precache-manifest.a4e3c5112cb4993bbe7c17be8bd61f08.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c883962962ef2c56dbdb",
+    "url": "/laoyoubang-website/css/app.e08bd1c8.css"
+  },
+  {
+    "revision": "a6d24108a8cb9c690caf80137bb3ce9f",
+    "url": "/laoyoubang-website/erweima.png"
+  },
+  {
+    "revision": "0d9028a3bb34d8b5e7df229b5e52686a",
+    "url": "/laoyoubang-website/img/1.0d9028a3.jpg"
+  },
+  {
+    "revision": "f016d549a66ac6a2a8612d90e4cb763c",
+    "url": "/laoyoubang-website/img/1.1.f016d549.png"
+  },
+  {
+    "revision": "630b2546e5e0e76c2bcba7edf2b026b3",
+    "url": "/laoyoubang-website/img/1.2.630b2546.png"
+  },
+  {
+    "revision": "73cd7fee474fec734db093d32147857f",
+    "url": "/laoyoubang-website/img/1.3.73cd7fee.png"
+  },
+  {
+    "revision": "dafb81b5cee4d944c0a7b127ec08b9b1",
+    "url": "/laoyoubang-website/img/1.4.dafb81b5.png"
+  },
+  {
+    "revision": "b3481659bb5ca9226650cb2cddf4319b",
+    "url": "/laoyoubang-website/img/1.5.b3481659.png"
+  },
+  {
+    "revision": "75355670be00cb07bc499555a7df2ed8",
+    "url": "/laoyoubang-website/img/1.6.75355670.png"
+  },
+  {
+    "revision": "9d56999b64a2c4c06edeb38fafe65724",
+    "url": "/laoyoubang-website/img/1.7.9d56999b.png"
+  },
+  {
+    "revision": "c3c98bcff4dfabea5002d9547b08c851",
+    "url": "/laoyoubang-website/img/1.8.c3c98bcf.png"
+  },
+  {
+    "revision": "bb90a2d1353d7019ad83bf8bf7c22cb0",
+    "url": "/laoyoubang-website/img/2.1.bb90a2d1.jpg"
+  },
+  {
+    "revision": "c73a6dfd0e89c408f9cc2c66a401ef80",
+    "url": "/laoyoubang-website/img/2.2.c73a6dfd.jpg"
+  },
+  {
+    "revision": "90bca3fe46f8e4d493d25ca1dfdb8d74",
+    "url": "/laoyoubang-website/img/2.3.90bca3fe.jpg"
+  },
+  {
+    "revision": "0489149c040222e569bcfd9bb268142e",
+    "url": "/laoyoubang-website/img/2.4.0489149c.jpg"
+  },
+  {
+    "revision": "c123b5c80f79721b7b0ed0ddbc4105de",
+    "url": "/laoyoubang-website/img/2.5.c123b5c8.jpg"
+  },
+  {
+    "revision": "e9b75316eae7f25b0c9a133212376910",
+    "url": "/laoyoubang-website/img/2.6.e9b75316.jpg"
+  },
+  {
+    "revision": "690fe42ecf692fd49c4458477084a1bf",
+    "url": "/laoyoubang-website/img/2.690fe42e.jpg"
+  },
+  {
+    "revision": "1f38bd8f4849a2cad8df599f589e736a",
+    "url": "/laoyoubang-website/img/3.1.1f38bd8f.jpg"
+  },
+  {
+    "revision": "70deae2c44188012334939abe1d2c59a",
+    "url": "/laoyoubang-website/img/3.2.70deae2c.jpg"
+  },
+  {
+    "revision": "45caf58ec9a0899757c57797aadfe184",
+    "url": "/laoyoubang-website/img/3.3.45caf58e.jpg"
+  },
+  {
+    "revision": "e55d5f1dd4a40565790251b7541a63dd",
+    "url": "/laoyoubang-website/img/3.4.e55d5f1d.jpg"
+  },
+  {
+    "revision": "b1d7407d08b5a9a8049e934922d70032",
+    "url": "/laoyoubang-website/img/3.b1d7407d.jpg"
+  },
+  {
+    "revision": "a8b3ac8f52ca561365757b5d737740ba",
+    "url": "/laoyoubang-website/img/4.a8b3ac8f.jpg"
+  },
+  {
+    "revision": "b9acb5a8d7b07b038203080484ed9f7f",
+    "url": "/laoyoubang-website/img/5.b9acb5a8.jpg"
+  },
+  {
+    "revision": "b2f37b9895f0431c6fa9fe0ea78cea43",
+    "url": "/laoyoubang-website/index.html"
+  },
+  {
+    "revision": "c883962962ef2c56dbdb",
+    "url": "/laoyoubang-website/js/app.b8c452e4.js"
+  },
+  {
+    "revision": "e5c87e9ac780c8fa11e2",
+    "url": "/laoyoubang-website/js/chunk-vendors.8e0864ac.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/laoyoubang-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/laoyoubang-website/robots.txt"
+  }
+]);
